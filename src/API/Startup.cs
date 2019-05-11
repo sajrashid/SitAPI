cb5955b9f1a6e6e2dff6d08308612a7613be27c2
Sajid Rashid <Sajid.Rashid@live.co.uk>
2019-05-11 04:26:04 +0000

    moved folder in src

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,210 +1,83 @@
-using AutoMapper;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Serilog;
-using API.Middleware;
-using API.Models;
-using API.Swagger;
-using Swashbuckle.AspNetCore.Filters;
+using Microsoft.AspNetCore.Authentication;
 using Swashbuckle.AspNetCore.Swagger;
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+using dotnetAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace API
+namespace dotnetAPI
 {
     public class Startup
-{
-    public Startup(IConfiguration configuration)
     {
-        Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        var appSettingsSection = Configuration.GetSection("AppSettings");
-        services.Configure<AppSettings>(appSettingsSection);
-        var appSettings = appSettingsSection.Get<AppSettings>();
-
-
-        services.AddWebServices(
-            BLLOptionsSection: Configuration.GetSection("AppSettings"),
-            DALOptionSection: Configuration.GetSection("ConnectionStrings")
-        );
-
-        services.AddAutoMapper();
-        services.AddHttpClient();
-
-        services.AddVersionedApiExplorer(opt =>
+        public Startup(IConfiguration configuration)
         {
-            opt.GroupNameFormat = "'v'VVV";
+            Configuration = configuration;
+        }
 
-                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
-                // can also be used to control the format of the API version in route templates
-                opt.SubstituteApiVersionInUrl = true;
-        });
+        public IConfiguration Configuration { get; }
 
-        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-        services.AddApiVersioning(opt =>
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            opt.ReportApiVersions = true;
-            opt.AssumeDefaultVersionWhenUnspecified = true;
-            opt.DefaultApiVersion = new ApiVersion(1, 0);
-            opt.ApiVersionReader = new UrlSegmentApiVersionReader();
-        });
 
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowSpecificOrigin",
-                builder => builder
-                .WithOrigins(appSettings.AllowedOrigins)
-                .AllowCredentials());
-        });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        services.AddSwaggerGen(opt =>
-        {
-            var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
-            foreach (var description in provider.ApiVersionDescriptions)
+         
+
+            services.AddDbContext<SitDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
             {
-                opt.SwaggerDoc(description.GroupName, GetSwaggerDocInfo(description));
-            }
-
-            opt.ExampleFilters();
-
-            var xmlPath = GetXmlDataAnnotationFilePath();
-            if (!string.IsNullOrEmpty(xmlPath))
-            {
-                opt.IncludeXmlComments(xmlPath);
-            }
-
-            opt.AddSecurityDefinition("oauth2", new ApiKeyScheme
-            {
-                Description = "Authorization header. Example: \"bearer {token}\"",
-                In = "header",
-                Name = "authorization",
-                Type = "apiKey"
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "SIT Testing API",
+                    Description = "API controller to talk to the testing DB",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "The Knight King",
+                        Email = string.Empty,
+                        Url = "https://got.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under MIT",
+                     
+                    }
+                });
             });
-            opt.OperationFilter<SecurityRequirementsOperationFilter>();
-            opt.OperationFilter<SwaggerDefaultValues>();
-        });
-
-        var key = Encoding.ASCII.GetBytes(appSettings.JwtSecretKey);
-        services.AddAuthentication(x =>
-        {
-            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddCookie(cfg => { cfg.SlidingExpiration = true; })
-        .AddJwtBearer(x =>
-        {
-            x.RequireHttpsMetadata = false;
-            x.SaveToken = true;
-            x.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateAudience = false,
-                ValidateIssuer = false,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key)
-            };
-        });
-
-        services.AddSwaggerExamples();
-
-        var logger = new LoggerConfiguration()
-           .Enrich.FromLogContext()
-           .ReadFrom.Configuration(Configuration);
-
-        Log.Logger = logger.CreateLogger();
-        Log.Information("web api service is started.");
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IOptionsMonitor<AppSettings> optionsAccessor, IApiVersionDescriptionProvider provider)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseHsts();
         }
 
-        app.UseCors("AllowSpecificOrigin");
-        app.UseAuthentication();
-        app.UseHttpContextMiddleware();
-
-        app.UseStaticFiles();
-
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            foreach (var description in provider.ApiVersionDescriptions.OrderByDescending(o => o.GroupName))
+            if (env.IsDevelopment())
             {
-                c.SwaggerEndpoint(
-                    $"/swagger/{description.GroupName}/swagger.json",
-                    description.GroupName.ToUpperInvariant());
+                app.UseDeveloperExceptionPage();
             }
-        });
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-        app.UseHttpsRedirection();
-        app.UseAuthentication();
-        app.UseMvc();
-    }
-
-    #region internal
-
-    private string GetXmlDataAnnotationFilePath()
-    {
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-        if (!File.Exists(xmlPath))
-        {
-            return null;
-        }
-
-        return xmlPath;
-    }
-
-    private Info GetSwaggerDocInfo(ApiVersionDescription description)
-    {
-        var info = new Info
-        {
-            Title = $"WebAPI {description.ApiVersion}",
-            Version = description.GroupName,
-            Description = "Web API Template",
-            Contact = new Contact()
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
             {
-                Name = "Web API service"
-            },
-            License = new License()
-            {
-                Name = "MIT"
-            }
-        };
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
-        if (description.IsDeprecated)
-        {
-            info.Description += $" {description.ApiVersion} API version is deprecated.";
+            app.UseMvc();
         }
-
-        return info;
     }
-
-    #endregion
-}
 }
